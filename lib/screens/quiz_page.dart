1dc8eb8bbcce36a4c1e25839583f86689ce3e689
Wilson Callisaya <demarktec@gmail.com>
2024-01-15 23:51:34 -0500

    vefificando respuesta e integrando preguntas dinamicas

--- lib/screens/quiz_page.dart
+++ lib/screens/quiz_page.dart
@@ -1,7 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:quizzapp/service/question_service.dart';
 
-class QuizPage extends StatelessWidget {
+class QuizPage extends StatefulWidget {
   const QuizPage({super.key});
+
+  @override
+  State<QuizPage> createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  QuestionService miQuiz = QuestionService();
+
+  List<Widget> puntaje = [];
+
+  checkAnswer(bool miRespuesta) {
+    bool correctAnswer = miQuiz.getQuestionAnswer();
+    if (correctAnswer == miRespuesta) {
+      puntaje.add(
+        Icon(
+          Icons.check,
+          color: Colors.greenAccent,
+        ),
+      );
+    } else {
+      puntaje.add(
+        Icon(
+          Icons.close,
+          color: Colors.redAccent,
+        ),
+      );
+    }
+    miQuiz.nextQuestion();
+
+    setState(() {});
+    print(puntaje);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,8 +46,63 @@ class QuizPage extends StatelessWidget {
         ),
         centerTitle: true,
         backgroundColor: Colors.black,
+        elevation: 10,
+        shadowColor: Colors.black,
       ),
-      body: Center(child: Text("hola")),
+      body: Container(
+        color: Colors.black,
+        child:
+            Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+          Expanded(
+              child: Center(
+            child: Container(
+              child: Text(
+                miQuiz.getQuestionText(),
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+            ),
+          )),
+          GestureDetector(
+            onTap: () {
+              checkAnswer(true);
+            },
+            child: Container(
+              margin: EdgeInsets.all(10),
+              height: 80,
+              color: Colors.green,
+              child: Center(
+                  child: Text(
+                "Verdadero",
+                style: TextStyle(fontWeight: FontWeight.w900),
+              )),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              checkAnswer(false);
+            },
+            child: Container(
+              margin: EdgeInsets.all(10),
+              height: 80,
+              color: Colors.red,
+              child: Center(
+                  child: Text(
+                "Falso",
+                style: TextStyle(fontWeight: FontWeight.w900),
+              )),
+            ),
+          ),
+          Row(
+            children: [
+              Icon(
+                Icons.close,
+                color: Colors.redAccent,
+              ),
+              ...puntaje
+            ],
+          ),
+        ]),
+      ),
     );
   }
 }
---
+++ lib/service/question_service.dart
@@ -0,0 +1,32 @@
+import 'package:quizzapp/service/question.dart';
+
+class QuestionService {
+  List<Question> questions = [
+    Question(
+        questionText: "Bedroom significa dormitorio", questionAnswer: true),
+    Question(
+        questionText: "Living room significa cocina", questionAnswer: false),
+    Question(questionText: "Kitchen significa cocina", questionAnswer: true),
+    Question(
+        questionText: "El verbo Tobe nos referimos al ser y estar",
+        questionAnswer: true),
+    Question(
+        questionText:
+            "In, On, Under, Next To nos indican posicion de un objeto dentro, encima, abajo y al costado respectivamente",
+        questionAnswer: true),
+  ];
+  int questionNumber = 0;
+  String getQuestionText() {
+    return questions[questionNumber].questionText;
+  }
+
+  bool getQuestionAnswer() {
+    return questions[questionNumber].questionAnswer;
+  }
+
+  nextQuestion() {
+    if (questionNumber < questions.length - 1) {
+      questionNumber++;
+    } 
+  }
+}
